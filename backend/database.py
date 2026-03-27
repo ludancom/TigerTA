8888c354d928ae880d5ab75defec84bcbe6a9a07
ludancom <la9361@princeton.edu>
2026-03-26 21:39:39 -0400

    SessionID things

--- backend/database.py
+++ backend/database.py
@@ -88,6 +88,7 @@ def queue_entry(session):
                 course = session['course']
                 assignment = session['assignment']
                 bug_description = session['bug_description']
+                session_id = 1
 
                 # Add student to student table
                 cursor.execute('''
