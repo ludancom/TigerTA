15689574d965247e4e52e55885135aa146853a8c
saleemadiallo <shaleemadiallo@gmail.com>
2026-03-29 12:36:07 -0400

    adding TA netid to session table once matched

--- backend/database.py
+++ backend/database.py
@@ -88,16 +88,7 @@ def queue_entry(session):
                     VALUES (%s, %s)
                 ''', [student_netid, student_name])
 
-                # Find place of student
-                # statement_str = """SELECT student_netid
-                # FROM student
-                #WHERE course = ?
-                #ORDER BY time_joined ASC
-                #"""
-                #cursor.execute(statement_str, (f"%{course}%"))
-                #table = cursor.fetchall()
-
-                # Add session to session table
+                # Add session to session table (TA will be added later once matched)
                 # Add TA back
                 cursor.execute('''
                 INSERT INTO session (session_id, student_netid, course, assignment, bug_description) 
@@ -108,14 +99,29 @@ def queue_entry(session):
     except Exception as ex:
         print("ERROR:", ex)
 
+# Find student place in queue 
+#def find_student_place():
+    #try:
+        #with contextlib.closing(psycopg.connect(DATABASE_URL)) as connection:
+            #with contextlib.closing(connection.cursor()) as cursor: 
+                # Find place of student
+                # statement_str = """SELECT student_netid
+                # FROM student
+                #WHERE course = ?
+                #ORDER BY time_joined ASC
+                #"""
+                #cursor.execute(statement_str, (f"%{course}%"))
+                #table = cursor.fetchall()
+    #except Exception as ex:
+        #print(f'{sys.argv[0]}: {ex}', file=sys.stderr)
+
 # Find name of the matched TA 
 def find_ta_name(session):
     try:
         with contextlib.closing(psycopg.connect(DATABASE_URL)) as connection:
             with contextlib.closing(connection.cursor()) as cursor: 
                 # Match student with a TA
-                course = session['course']
-                ta_netid = find_ta_netid(course)
+                ta_netid = find_ta_netid(session)
 
                 # Get TA name
                 statement_str = """SELECT ta_name 
@@ -132,10 +138,15 @@ def find_ta_name(session):
         print(f'{sys.argv[0]}: {ex}', file=sys.stderr)
 
 # Find netid of a TA that is available and teaches course, update TA availability
-def find_ta_netid(course):
+def find_ta_netid(session):
     try:
         with contextlib.closing(psycopg.connect(DATABASE_URL)) as connection:
             with contextlib.closing(connection.cursor()) as cursor:
+                # Get student's course and netid
+                course = session['course']
+                student_netid = session['student_netid']
+
+                # Get TA netid
                 statement_str = """SELECT ta.ta_netid
                 FROM ta, ta_courses
                 WHERE ta_courses.course = %s
@@ -144,9 +155,18 @@ def find_ta_netid(course):
                 cursor.execute(statement_str, (course,))
                 table = cursor.fetchall()
                 ta_netid = table[0][0]
-                statement_str = """UPDATE ta SET available = FALSE
-                WHERE ta_netid=%s"""
+
+                # Set TA to unavailable
+                statement_str = """UPDATE ta 
+                SET available = FALSE
+                WHERE ta_netid = %s"""
                 cursor.execute(statement_str, (ta_netid,))
+
+                # Add TA's netid to session table
+                statement_str = """UPDATE session 
+                SET ta_netid = %s
+                WHERE student_netid = %s"""
+                cursor.execute(statement_str, (ta_netid, student_netid))
 
                 connection.commit()
                 return ta_netid
