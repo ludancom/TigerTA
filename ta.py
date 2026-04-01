c23f85dceaf833bc8ec3ae47b605befbeb444add
amel-osman <132000199+amel-osman@users.noreply.github.com>
2026-04-01 16:03:49 -0400

    in session and end session ta functions

--- ta.py
+++ ta.py
@@ -83,29 +83,30 @@ def workhub():
     
     if flask.request.method == 'POST':
         
-        # Get the user's clock in status
+        # Get the user's button request
         action = flask.request.form.get('action')
 
         # Update the TA's attendance when they clock in
         ### Implement this function later ###
         if action == clock_in:
-            database.clockin(ta_netid)
+            database.clock_in(ta_netid)
 
         # If the TA wants to start a session...
         if action == start_session:
-            session_info = database.start_session()
+            session_info = database.start_session(ta_netid)
             # This can all be done in one function in database.py
 
             # Change the TA's availability to true
 
-            # Check if there are students in the queue
+            # Check if the TA has gotten matched
                 # pull everyone from sessions table where ta_netid = self tas netid
                 # get and return the student's information (name, course, assignment, bug description) that they were matched with from the sessions table
+                # IF matched.... redirect to the in session page
 
-            # If there are no students in the queue:
+            # If not matched yet...
                 # Refresh and check again every 5 seconds or so (this is done in JS on frontend)
 
-        # Set student name cookie
+        # Set session info cookie
         response.set_cookie('session_info', session_info)
 
     return flask.render_template('workhub.html')
@@ -115,21 +116,81 @@ def workhub():
 #-----------------------------------------------------------------------
 @ta_routes.route('/insessionta', methods=['GET', 'POST'])
 def insessionta():
-    """ Method that displays the TA the student was matched with and
-    their bug description. """
+    """ Method that displays the student the TA was matched with and
+    their session details. """
+
+    # Get session info cookie
+    session_info = flask.request.cookies.get('session_info')
+
+    # Get student info
+    student_name = session_info['student_name']
+
+    # Get course
+    course = session_info['course']
+
+    # Get assignment
+    assignment = session_info['assignment']
+
+    # Get bug description
+    bug_description = session_info['bug_description']
+
+    # Display in session page
+    html_code = flask.render_template('insessionta.html', 
+    student_name = student_name, course = course, 
+    assignment = assignment, bug_description = bug_description)
+
+    response = flask.make_response(html_code)
+
+    # End session button takes them to next page
+    if flask.request.method == 'POST':
+
+        # Get the user's button request
+        action = flask.request.form.get('action')
+
+        # If the user presses the end session button, it redirects 
+        # them  to the end session page
+        if action == end_session:
+
+            # Remove the session from the queue after session ends
+            database.remove_session()
+
+            # Redirect to the end session page
+            response = flask.redirect('/endsessionta')
+
+            # Set student name cookie for the end session page
+            response.set_cookie('student_name', student_name)
+
+    return response
+
+
+#-----------------------------------------------------------------------
+# End Session TA Page:
+#-----------------------------------------------------------------------
+@ta_routes.route('/endsessionta', methods=['GET', 'POST'])
+def endsessionta():
+    """ Method that displays the end page, the student's name, and
+    a button to return back to home. """
 
     # Get student name cookie
     student_name = flask.request.cookies.get('student_name')
 
-    # Get course
-    # Get assignment
-    # Get bug description
-    # Display these things
+    # Display end session page
+    html_code = flask.render_template('endsessionta.html', 
+    student_name = student_name)
 
-    # Display in session page
-    html_code = flask.render_template('insessionta.html', bug_description = bug_description, student_name = ta_name)
     response = flask.make_response(html_code)
 
-    # End session button takes them to next page
-    
+    # Back to work hub page if they press "Home" button
+    if flask.request.method == 'POST':
+
+        # Get the user's button request
+        action = flask.request.form.get('action')
+
+        # If the user presses the end session button, it redirects 
+        # them  to the end session page
+        if action == home:
+
+            # Redirect to the work hub  page
+            response = flask.redirect('/workhub')
+
     return response
