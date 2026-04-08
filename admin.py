5fe0fb27509ffcd96969e4664c13013f4f1fe6a9
amberkhatib2028 <ak1225@princeton.edu>
2026-04-08 17:21:02 -0400

    implemented add_ta and remove_ta

--- admin.py
+++ admin.py
@@ -19,7 +19,7 @@ admin_routes = flask.Blueprint('admin_routes', __name__, template_folder='.')
 @admin_routes.route('/', methods={'GET'})
 @admin_routes.route('/home', methods={'GET'})
 def homepage():
-    """ Method that displays the homepage page to students. """
+    """ Method that displays the homepage page to administrators. """
 
     # Send users to the HTML home page
     html_code = flask.render_template('homepage.html')
@@ -27,3 +27,56 @@ def homepage():
 
     return response
 
+@admin_routes.route('/add_ta', methods=['GET', 'POST'])
+def add_ta():
+    """ Method that adds a TA to the database. """
+
+    # Authenticate CAS
+    auth.authenticate()
+
+    # Get the user's net id from CAS
+    net_id = auth.get_username()
+
+    if role == 'admin':
+
+        if flask.request.method == 'POST':
+
+        # Get the TA's net id from the form
+        ta_net_id = flask.request.form.get('ta_net_id')
+
+        # Add the TA to the database
+        database.add_ta(ta_net_id)
+
+        # Send the user to a confirmation page
+        response = flask.redirect(flask.url_for('admin_routes.view_tas'))
+
+@admin_routes.route('/remove_ta', methods=['GET', 'POST'])
+def remove_ta():
+    """ Method that removes a TA from the database. """
+
+    # Authenticate CAS
+    auth.authenticate()
+
+    # Get the user's net id from CAS
+    net_id = auth.get_username()
+
+    if flask.request.method == 'POST':
+
+        # Get the TA's net id from the form
+        ta_net_id = flask.request.form.get('ta_net_id')
+
+        # Remove the TA from the database
+        database.remove_ta(ta_net_id)
+
+        # Send the user to a confirmation page
+        response = flask.redirect(flask.url_for('admin_routes.view_tas'))
+
+@admin_routes.route('/view_tas', methods=['GET', 'POST'])
+def view_tas():
+    """ Method that displays the list of TAs to the user. """
+
+    # Send users to the HTML page with the list of TAs
+    html_code = flask.render_template('view_tas.html')
+    response = flask.make_response(html_code)
+
+    return response
