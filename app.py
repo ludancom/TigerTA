f84ed93b93fe6727bbd0f1c9ba9e1e8cc3513a65
Amber Khatib <ak1225@princeton.edu>
2026-04-22 14:30:20 -0400

    added notifications specifications

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from student import student_routes
 from ta import ta_routes
 from admin import admin_routes
 import flask_wtf.csrf
+import notifications
 
 #-----------------------------------------------------------------------
 # Create app
@@ -27,6 +28,16 @@ app = flask.Flask(
 dotenv.load_dotenv()
 _APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
 app.secret_key = _APP_SECRET_KEY
+
+# Mail config
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+
+notifications.mail.init_app(app)
 auth.init(app)
 
 # Security Measures
