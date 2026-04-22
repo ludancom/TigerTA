a163dd82b83345ff22c87df0d75c22f458625bfe
Mohemeen Ahmed <ma2244@princeton.edu>
2026-04-22 15:53:15 -0400

    back button

--- admin.py
+++ admin.py
@@ -75,7 +75,7 @@ def edit_ta():
 
     ta_net_id = flask.request.form.get('ta_netid', '').strip()
     ta_name = flask.request.form.get('ta_name', '').strip()
-    ta_email = flask.request.form.get('email', '').strip()
+    ta_email = f'{ta_net_id}@princeton.edu'
     courses = flask.request.form.get('courses', '').strip()
 
     database.edit_ta(ta_net_id, ta_name, ta_email, courses)
