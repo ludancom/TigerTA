6044f87c951d4b4d95b75306758b89831575da54
saleemadiallo <shaleemadiallo@gmail.com>
2026-04-23 17:14:50 -0400

    fixing

--- googlesheet.py
+++ googlesheet.py
@@ -3,11 +3,11 @@ import gspread
 client = gspread.service_account(filename='tigerta-d62e8c569ae9.json')
 # Worksheet for shifts
 shiftSheet = client.open_by_key(key='1yT4kDQ-0aeV7Rac9R7TxEHTyS-BIPbrgayMMFtSCAt4')
-shiftworksheet = sheet.get_worksheet_by_id(id=0)
+shiftworksheet = shiftSheet.get_worksheet_by_id(id=0)
 
 # Worksheet for feedback
-shiftSheet = client.open_by_key(key='1yT4kDQ-0aeV7Rac9R7TxEHTyS-BIPbrgayMMFtSCAt4')
-shiftworksheet = sheet.get_worksheet_by_id(id=1)
+#shiftSheet = client.open_by_key(key='1yT4kDQ-0aeV7Rac9R7TxEHTyS-BIPbrgayMMFtSCAt4')
+#shiftworksheet = shiftSheet.get_worksheet_by_id(id=1)
 
 def log_shift(netid, name, date, clock_in, clock_out, students):
     worksheet.append_row([
@@ -19,8 +19,8 @@ def log_shift(netid, name, date, clock_in, clock_out, students):
         students
     ])
 
-def log_feedback(netid, feedback):
-    worksheet.append_row([
-        netid,
-        feedback
-    ])
+#def log_feedback(netid, feedback):
+    #worksheet.append_row([
+     #   netid,
+     #   feedback
+   # ])
