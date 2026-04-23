3caa3320f295e7858df94994dae30375a9d95cf3
saleemadiallo <shaleemadiallo@gmail.com>
2026-04-23 16:57:22 -0400

    adding feedback worksheet to google sheet

--- googlesheet.py
+++ googlesheet.py
@@ -1,8 +1,13 @@
 import gspread
 
 client = gspread.service_account(filename='tigerta-d62e8c569ae9.json')
-sheet = client.open_by_key(key='1yT4kDQ-0aeV7Rac9R7TxEHTyS-BIPbrgayMMFtSCAt4')
-worksheet = sheet.get_worksheet_by_id(id=0)
+# Worksheet for shifts
+shiftSheet = client.open_by_key(key='1yT4kDQ-0aeV7Rac9R7TxEHTyS-BIPbrgayMMFtSCAt4')
+shiftworksheet = sheet.get_worksheet_by_id(id=0)
+
+# Worksheet for feedback
+shiftSheet = client.open_by_key(key='1yT4kDQ-0aeV7Rac9R7TxEHTyS-BIPbrgayMMFtSCAt4')
+shiftworksheet = sheet.get_worksheet_by_id(id=1)
 
 def log_shift(netid, name, date, clock_in, clock_out, students):
     worksheet.append_row([
@@ -12,4 +17,10 @@ def log_shift(netid, name, date, clock_in, clock_out, students):
         clock_in,
         clock_out,
         students
+    ])
+
+def log_feedback(netid, feedback):
+    worksheet.append_row([
+        netid,
+        feedback
     ])
