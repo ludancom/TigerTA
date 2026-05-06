6084b686b96ad21e428b85810f0aa84cfe1de8df
saleemadiallo <shaleemadiallo@gmail.com>
2026-05-05 23:30:18 -0400

    does this work?

--- database.py
+++ database.py
@@ -144,9 +144,15 @@ def queue_entry(session):
                 VALUES (%s, %s, %s, %s)
                 ''', [student_netid, course, assignment, bug_description])
                 connection.commit()
+            
+        # queue_entry is the single source of truth for triggering the
+        # "you're next in line" path. notify_next_in_line itself decides
+        # whether to actually send (no-op if the position-1 student was
+        # already notified for this session).
+        notify_next_in_line(course)
 
-                # If student is successfully added to queue...
-                return True
+        # If student is successfully added to queue...
+        return True
 
     except Exception as ex:
         print(f'{sys.argv[0]}: {ex}', file=sys.stderr)
